b8cb75163b689d9eec8b9bebe747c26358fab1a4
bibhasborah <bborah@usc.edu>
2026-07-18 09:26:22 -0700

    Date out

--- app/page.tsx
+++ app/page.tsx
@@ -538,7 +538,7 @@ export default function Home() {
 
             <div>
               <h3 className="text-2xl font-bold text-amber-700">
-                Day 1
+                Aug 28, 2026
               </h3>
 
               <p className="mt-2 text-slate-600">
@@ -548,7 +548,7 @@ export default function Home() {
 
             <div>
               <h3 className="text-2xl font-bold text-amber-700">
-                Day 2
+                Aug 29, 2026
               </h3>
 
               <p className="mt-2 text-slate-600">
@@ -558,7 +558,7 @@ export default function Home() {
 
             <div>
               <h3 className="text-2xl font-bold text-amber-700">
-                Day 3
+                Aug 30, 2026
               </h3>
 
               <p className="mt-2 text-slate-600">
